a3d7c2a22b9decd7027e322621f50e18f7761327
Jiwon <googims330@gmail.com>
2019-12-18 01:00:02 +0900

    가장 긴 증가하는 부분 수열

--- prac_11053/Main.java
+++ prac_11053/Main.java
@@ -5,7 +5,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
 
 public class Main {
 
@@ -15,27 +14,29 @@ public class Main {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int n = Integer.parseInt(br.readLine());
+
+		String[] in = br.readLine().split(" ");
+		int[] input = new int[n + 1];
 		int[] dp = new int[1001];
 
-		String str = br.readLine();
-		StringTokenizer tok = new StringTokenizer(str, " ");
-
-		int before = Integer.parseInt(tok.nextToken());
-		
-		int dif = before;
-		dp[1] = 1;
-
-		for (int i = 2; i <= n; i++) {
-			int present = Integer.parseInt(tok.nextToken());
-			if ((present - before) > 0 && dif <= (present - before)) {
-				dif = present - before;
-				dp[i] = dp[i - 1] + 1;
-			} else
-				dp[i] = dp[i - 1];
-
-			before = present;
+		for (int i = 0; i < n; i++) {
+			input[i + 1] = Integer.parseInt(in[i]);
+			dp[i + 1] = 1;
 		}
-		bw.write(String.valueOf(dp[n]));
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				if (input[i] > input[j] && dp[i] <= dp[j]) {
+					dp[i] = dp[j] + 1;
+				}
+			}
+		}
+
+		int max = 0;
+		for (int i = 1; i <= n; i++) {
+			max = Math.max(max, dp[i]);
+		}
+		bw.write(String.valueOf(max));
 
 		bw.flush();
 		bw.close();
