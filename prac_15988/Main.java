916a22fd0dad40607e2c0a5f8b142385c8b65a09
Jiwon <googims330@gmail.com>
2019-12-17 17:16:04 +0900

    1,2,3으로 n만들기

---
+++ prac_15988/Main.java
@@ -0,0 +1,38 @@
+package prac_15988;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	static final long mod = 1000000009;
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		long[] dp = new long[1000001];
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 4;
+		
+		int t = Integer.parseInt(br.readLine());
+		for (int i = 0; i < t; i++) {
+			int n = Integer.parseInt(br.readLine());
+
+			for (int j = 4; j <= n; j++) {
+				dp[j] = (dp[j - 1] % mod) + (dp[j - 2] % mod) + (dp[j - 3] % mod);
+				dp[j] %= mod;
+			}
+			bw.write(String.valueOf(dp[n])+"\n");
+		}
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
