0bd5cfeabb73190cad4f0e5e0f12c6351ece60be
Jiwon <googims330@gmail.com>
2019-12-31 00:21:41 +0900

    dfs, 사용자정의 배열,  n번째 수보다 n+1번째 수가 크면 된다.

---
+++ prac_15655/Main.java
@@ -0,0 +1,43 @@
+package prac_15655;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n, m;
+	static int[] arr;
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+
+		st = new StringTokenizer(br.readLine());
+		arr = new int[n];
+
+		for (int i = 0; i < n; i++)
+			arr[i] = Integer.parseInt(st.nextToken());
+
+		Arrays.sort(arr);
+		dfs(0, 0, "");
+
+		br.close();
+	}
+
+	public static void dfs(int idx, int len, String str) {
+		if (len == m) {
+			System.out.println(str);
+			return;
+		}
+		for (int i = 0; i < n; i++) {
+			if (arr[i] > idx)
+				dfs(arr[i], len + 1, str + arr[i] + " ");
+		}
+	}
+
+}
