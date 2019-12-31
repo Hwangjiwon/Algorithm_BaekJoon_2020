725ef50f552675037528eeb310d6e195657ecf22
Jiwon <googims330@gmail.com>
2019-12-31 22:09:13 +0900

    dfs,  a = new ArrayList<Integer>(s);
    메인에서 넣고, dfs돌리기

--- prac_15663/Main.java
+++ prac_15663/Main.java
@@ -3,54 +3,53 @@ package prac_15663;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Set;
 import java.util.StringTokenizer;
+import java.util.TreeSet;
 
 public class Main {
-	static int n, m;
-	static int[] a;
-	static HashSet<String> h = new HashSet<String>();
-	static int visited[] = new int[10001];
+   static int n, m;
+   static ArrayList<Integer> a;
+    static int visited[] = new int[10001];
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+   public static void main(String[] args) throws IOException {
+      // TODO Auto-generated method stub
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      StringTokenizer st = new StringTokenizer(br.readLine());
 
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
+      n = Integer.parseInt(st.nextToken());
+      m = Integer.parseInt(st.nextToken());
 
-		st = new StringTokenizer(br.readLine());
-		a = new int[n];
-		for (int i = 0; i < n; i++) {
-			a[i] = Integer.parseInt(st.nextToken());
-			visited[a[i]]++;
-		}
+      st = new StringTokenizer(br.readLine());
+      Set<Integer> s = new TreeSet<Integer>();
+        for(int i=0; i<n; i++) {
+            int t = Integer.parseInt(st.nextToken());
+            s.add(t);
+            visited[t]++;
+        }
+        
+        a = new ArrayList<Integer>(s);
+        
+      dfs(0, "");
+      
+      
+      br.close();
+   }
 
-		Arrays.sort(a);
-
-		dfs(0, "");
-
-		br.close();
-	}
-
-	public static void dfs(int len, String str) {
-		if (len == m) {
-			if (!h.contains(str)) {
-				h.add(str);
-				System.out.println(str);
-			}
-			return;
-		}
-
-		for (int i = 0; i < n; i++) {
-			if (visited[a[i]] > 0) {
-				visited[a[i]]--;
-				dfs(len + 1, str + a[i] + " ");
-				visited[a[i]]++;
-			}
-		}
-	}
+   public static void dfs(int len, String str) {
+      if(len == m) {
+            System.out.println(str);
+            return;
+        }
+        
+        for(int i=0; i<a.size(); i++) {
+            if(visited[a.get(i)] > 0) {
+                visited[a.get(i)]--;
+                dfs(len + 1, str + a.get(i) + " ");
+                visited[a.get(i)]++;
+         }
+      }
+   }
 
 }
