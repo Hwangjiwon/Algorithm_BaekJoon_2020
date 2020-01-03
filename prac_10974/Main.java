fcdf71f44c04dada5b630109e1b8feff2598ce68
Jiwon <googims330@gmail.com>
2020-01-03 15:24:43 +0900

    차이를 최대로하는 순열

---
+++ prac_10974/Main.java
@@ -0,0 +1,62 @@
+package prac_10974;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	static int n;
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++)
+			arr[i] = i + 1;
+
+		for (int i = 0; i < n; i++)
+			System.out.print(arr[i] + " ");
+		while (true) {
+			if (nextPerm(arr) == null)
+				break;
+		}
+		br.close();
+	}
+
+	public static int[] nextPerm(int[] arr) {
+		int[] result = new int[n];
+		for (int i = 0; i < n; i++)
+			result[i] = arr[i];
+
+		int idx = -1;
+		for (int i = 0; i < n - 1; i++) {
+			if (result[i] < result[i + 1])
+				idx = i;
+		}
+		if (idx < 0) {
+			System.out.println(-1);
+			return null;
+		}
+		for (int i = n - 1; i > idx; i--) {
+			if (result[idx] < result[i]) {
+				int tmp = result[idx];
+				result[idx] = result[i];
+				result[i] = tmp;
+				break;
+			}
+		}
+
+		for (int i = idx + 1; i < (n + idx + 1) / 2; i++) {
+			int tmp = result[i];
+			result[i] = result[n - (i - idx)];
+			result[n - (i - idx)] = tmp;
+		}
+
+		for (int i = 0; i < n; i++) {
+			System.out.print(result[i] + " ");
+		}
+		return result;
+	}
+}
