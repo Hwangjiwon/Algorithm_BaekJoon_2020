0580142ca827796de60197c834e0cb8bfffc0745
Jiwon <googims330@gmail.com>
2020-01-05 23:06:04 +0900

    외판원 dfs 최소거리비용

--- prac_10971/Main.java
+++ prac_10971/Main.java
@@ -8,54 +8,51 @@ import java.util.StringTokenizer;
 public class Main {
 	static int n;
 	static int[][] w;
-	static int[] check;
 	static boolean[] visited;
+	static int min = Integer.MAX_VALUE; // Integer의 최대값을 min으로 세팅
+	static int start = 0; 
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
 		w = new int[n][n];
-		check = new int[n];
-		visited = new boolean[n];
-
-		StringTokenizer st;
 		for (int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < n; j++) {
 				w[i][j] = Integer.parseInt(st.nextToken());
 			}
-			check[i] = i;
 		}
 
-		dfs(0, check);
+		visited = new boolean[n];
+		for (int i = 0; i < n; i++) {
+			start = i;
+			solve(i, i, 0, 0);
+		}
+		System.out.println(min);
 
 		br.close();
 	}
 
-	public static void dfs(int len, int[] arr) {
-		if (len == n) {
-			print(arr);
+	private static void solve(int x, int y, int len, int sum) {
+		if (len == n && start == y) {
+			min = Math.min(min, sum);
 			return;
 		}
 
-		for (int i = len; i < n; i++) {
-			swap(arr,len,i);
-			dfs(len + 1, arr);
-			swap(arr,len,i);
-		}
-	}
-
-	public static void swap(int[] arr, int len, int i) {
-		int tmp = arr[len];
-		arr[len] = arr[i];
-		arr[i] = tmp;
-	}
-
-	public static void print(int[] arr) {
 		for (int i = 0; i < n; i++) {
-			System.out.print(arr[i]);
+			if (visited[i])
+				continue;
+			// y->i로 갈 수 없는 경우 0값을 가지고있다.
+			// 따라서 y==i 일 경우만 체크해주면 틀린 값을 얻게된다.
+			if (w[y][i] == 0)
+				continue;
+			if (sum + w[y][i] > min)
+				continue;
+			visited[i] = true;
+			solve(y, i, len + 1, sum + w[y][i]);
+			visited[i] = false;
 		}
-		System.out.println();
 	}
+
 }
