2139c37d3cf991b88edfb24433c62e864fb6393f
Jiwon <googims330@gmail.com>
2020-01-07 15:05:31 +0900

    yet

--- prac_14501/Main.java
+++ prac_14501/Main.java
@@ -3,6 +3,7 @@ package prac_14501;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 	static int[] t;
@@ -11,13 +12,20 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		int n = Integer.parseInt(br.readLine());
-		String[] str = br.readLine().split(" ");
-		for(int i = 0; i < n; i++) {
-			
+		
+		t = new int[n+6];
+		p = new int[n+6];
+		StringTokenizer st;
+		for(int i = 1; i <= n; i++) {
+			st = new StringTokenizer(br.readLine());
+			t[i] = Integer.parseInt(st.nextToken());
+			p[i] = Integer.parseInt(st.nextToken());
 		}
 		
+		for(int i = 1; i <= n; i++) {
+			
+		}
 		br.close();
 	}
 
