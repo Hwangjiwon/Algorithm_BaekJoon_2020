4ae454a452c975c150815c7e513ac6a979737926
Jiwon <googims330@gmail.com>
2020-01-07 12:54:12 +0900

    퇴사 yet

---
+++ prac_14501/Main.java
@@ -0,0 +1,24 @@
+package prac_14501;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	static int[] t;
+	static int[] p;
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		String[] str = br.readLine().split(" ");
+		for(int i = 0; i < n; i++) {
+			
+		}
+		
+		br.close();
+	}
+
+}
