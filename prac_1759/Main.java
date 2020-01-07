a92ce2244ff5fddd5a6d7fe18d3584e7f61f09ee
Jiwon <googims330@gmail.com>
2020-01-07 12:33:28 +0900

    암호만들기 dfs, idx로 크기비교

--- prac_1759/Main.java
+++ prac_1759/Main.java
@@ -9,8 +9,10 @@ import java.util.StringTokenizer;
 public class Main {
 	static int l; // r
 	static int c; // n
-	static String[] arr;
+	static char[] arr;
 	static boolean[] visited;
+	static int mo = 0;
+	static int ja = 0;
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
@@ -19,34 +21,52 @@ public class Main {
 
 		l = Integer.parseInt(st.nextToken());
 		c = Integer.parseInt(st.nextToken());
-		arr = new String[c];
+		arr = new char[c];
 		visited = new boolean[c];
 		String str = br.readLine();
 		for (int i = 0; i < c; i++) {
-			arr[i] = String.valueOf(str.charAt(i * 2));
+			arr[i] = str.charAt(i * 2);
 		}
 
 		Arrays.sort(arr);
 
-		dfs(0, 0, "");
+		dfs(0, 0, 0, "");
 		br.close();
 	}
 
-	public static void dfs(int n, int r, String str) {
+	public static void dfs(int idx, int n, int r, String str) {
 		if (r == l) {
-			System.out.println(str);
+			if (check(str) == true)
+				System.out.println(str);
 			return;
 		}
 
 		for (int i = 0; i < c; i++) {
-			if (arr[i].compareTo(arr[r]) < 0)
+			if(visited[i])
 				continue;
-			if (!visited[i]) {
+			if (arr[i] >= arr[idx]) {
 				visited[i] = true;
-				dfs(n + 1, r + 1, str + arr[i]);
+				dfs(i, n + 1, r + 1, str + arr[i]);
 				visited[i] = false;
 			}
 		}
+	}
+
+	public static boolean check(String str) {
+		ja = 0;
+		mo = 0;
+		for (int i = 0; i < l; i++) {
+			if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o'
+					|| str.charAt(i) == 'u') // 모음 1개
+				ja++;
+			else // 자음 2개이상
+				mo++;
+		}
+
+		if (ja >= 1 && mo >= 2)
+			return true;
+		else
+			return false;
 	}
 
 }
