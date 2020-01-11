9addbddb071fa76236d118b5cb168ee8f1f1696d
Jiwon <googims330@gmail.com>
2020-01-12 01:05:34 +0900

    부등호, dfs

--- prac_2529/Main.java
+++ prac_2529/Main.java
@@ -3,64 +3,63 @@ package prac_2529;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 	static int k;
-	static int[] arr;
-	static String result;
+	static String[] compare;
 	static boolean visited[];
-	static int max = Integer.MIN_VALUE;
-	static int min = Integer.MAX_VALUE;
+	static List<String> result = new ArrayList<>();
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		k = Integer.parseInt(br.readLine());
-		String[] str = br.readLine().split(" ");
+		compare = br.readLine().split(" ");
 
-		for (int i = 0; i < k; i++) {
-			System.out.println(str[i]);
-		}
-
-		// 1 < 1 : 2
-		// 1 < 1 < 1 : 3
-		// 1 < 1 < 1 < 1 : 4
-		arr = new int[k + 1];
 		visited = new boolean[10];
-
+		
 		dfs(0, "");
+		
+		System.out.println(result.get(result.size()-1));
+		System.out.println(result.get(0));
+		
 		br.close();
 	}
 
 	public static void dfs(int len, String str) {
 		if (len == k + 1) {
-			//check
-			if(check(str)) {
-				result = str;
-				System.out.println(result);
+			if (check(str) == true) {
+				result.add(str);
 			}
-			
-			
-			System.out.println(str);
-			return;
 		}
 
 		for (int i = 0; i <= 9; i++) {
 			if (!visited[i]) {
-				visited[i] = true;				
+				visited[i] = true;
 				dfs(len + 1, str + i);
 				visited[i] = false;
 			}
 		}
 	}
-	
+
 	public static boolean check(String str) {
-		
-		return false;
-		
-		
-		
+		for (int i = 1; i < str.length(); i++) {
+			int j = i - 1;
+			
+			if (compare[j].equals("<")) {
+				if (!(str.charAt(i - 1) - '0' < str.charAt(i) - '0')) {
+					return false;
+				}
+			} else if (compare[j].equals(">")) {
+				if (!(str.charAt(i - 1) - '0' > str.charAt(i) - '0')) {
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 
 }
