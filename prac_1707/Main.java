e177d2a4a15c35696b5d4b7fa427f39bd16da989
Jiwon <googims330@gmail.com>
2020-01-21 00:07:38 +0900

    이분그래프 dfs

--- prac_1707/Main.java
+++ prac_1707/Main.java
@@ -4,13 +4,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
 	static ArrayList<ArrayList<Integer>> graph;
-	static int[] color; // color: 1, -1
-	static boolean[] visited;
 	static boolean chk;
 	static int v, e;
+	static int[] color; // color: 1, -1
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
@@ -23,11 +23,12 @@ public class Main {
 			e = Integer.parseInt(str[1]); // °£¼±
 
 			graph = new ArrayList<>();
-			visited = new boolean[v + 1];
 			color = new int[v + 1];
+			chk = true;
 
-			for (int j = 1; j <= v; j++) {
-				graph.add(new ArrayList<>());
+			for (int j = 0; j <= v; j++) {
+				Arrays.fill(color, 0);
+				graph.add(new ArrayList<Integer>());
 			}
 
 			while (e-- > 0) {
@@ -43,26 +44,27 @@ public class Main {
 				if (!chk)
 					break;
 
-				if (visited[j] == false) {
-					color[j] = 1;
-					dfs(j, 1);
+				if (color[i] == 0) {
+					dfs(i, 1);
 				}
 			}
+			System.out.print(chk ? "YES" : "NO");
 		}
-		System.out.println(chk ? "YES" : "NO");
+		System.out.println();
 		br.close();
 	}
 
-	public static void dfs(int node, int col) {
-		for (int i = 1; i <= v; i++) {
-			if (color[i] == col) {
+	public static void dfs(int node, int c) {
+		color[node] = c;
+
+		for (int adjNode : graph.get(node)) {
+			if (color[adjNode] == c) {
 				chk = false;
 				return;
 			}
 
-			if (visited[i] == false && graph.get(node).get(i) != col) {
-				visited[i] = true;
-				dfs(i, -col);
+			if (color[adjNode] == 0) { // not visited
+				dfs(adjNode, -c);
 			}
 		}
 
