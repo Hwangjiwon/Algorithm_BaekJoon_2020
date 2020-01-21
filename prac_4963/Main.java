fcdbe71f5e6eb1dc245dbed46d795418da37a747
Jiwon <googims330@gmail.com>
2020-01-21 15:17:32 +0900

    섬의 개수, dfs, bfs

--- prac_4963/Main.java
+++ prac_4963/Main.java
@@ -3,13 +3,15 @@ package prac_4963;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
 	static int w, h, cnt;
 	static int[][] graph;
 	static boolean[][] visited;
-	static int[] dx = { 0, 0, -1, 1, -1, -1, 1, 1 }; //상하좌우 좌상 좌하 우상 우하
-    static int[] dy = { 1, -1, 0, 0, 1, -1, 1, -1 };
+	static int[] dx = { 0, 0, -1, 1, -1, -1, 1, 1 }; // 상하좌우 좌상 좌하 우상 우하
+	static int[] dy = { 1, -1, 0, 0, 1, -1, 1, -1 };
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
@@ -17,8 +19,8 @@ public class Main {
 		String[] str;
 		while (true) {
 			str = br.readLine().split(" ");
-			w = Integer.parseInt(str[0]); //너비
-			h = Integer.parseInt(str[1]); //높이
+			w = Integer.parseInt(str[0]); // 너비
+			h = Integer.parseInt(str[1]); // 높이
 			if (w == 0 && h == 0)
 				break;
 
@@ -38,7 +40,8 @@ public class Main {
 				for (int j = 0; j < w; j++) {
 					if (graph[i][j] == 1 && !visited[i][j]) {
 						cnt++;
-						dfs(i, j);
+						//dfs(i, j);
+						bfs(i, j);
 					}
 				}
 			}
@@ -61,8 +64,31 @@ public class Main {
 			}
 		}
 	}
-	
+
 	public static void bfs(int y, int x) {
-		
+		Queue<Integer> qx = new LinkedList<Integer>();
+		Queue<Integer> qy = new LinkedList<Integer>();
+
+		visited[y][x] = true;
+		qx.add(x);
+		qy.add(y);
+
+		int tmpx;
+		int tmpy;
+		while (!qx.isEmpty() && !qy.isEmpty()) {
+			tmpx = qx.poll();
+			tmpy = qy.poll();
+
+			for (int i = 0; i < 8; i++) {
+				x = tmpx + dx[i];
+				y = tmpy + dy[i];
+
+				if (x >= 0 && y >= 0 && x < w && y < h) {
+					if (graph[y][x] == 1 && visited[y][x] == false) {
+						bfs(y, x);
+					}
+				}
+			}
+		}
 	}
 }
