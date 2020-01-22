e4e40339f18083ae1a2e6d9514fe6c7d1dbfc20b
Jiwon <googims330@gmail.com>
2020-01-22 12:45:04 +0900

    미로탐색 bfs

---
+++ prac_2178/Main.java
@@ -0,0 +1,72 @@
+package prac_2178;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+	static int n, m;
+	static int[][] graph;
+	static boolean[][] visited;
+	static int[] dx = { 0, 0, -1, 1 };
+	static int[] dy = { 1, -1, 0, 0 };
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] str = br.readLine().split(" ");
+		n = Integer.parseInt(str[0]); // y√‡
+		m = Integer.parseInt(str[1]); // x√‡
+
+		graph = new int[n + 1][m + 1];
+		visited = new boolean[n + 1][m + 1];
+
+		for (int i = 0; i < n; i++) {
+			str = br.readLine().split("");
+			for (int j = 0; j < m; j++) {
+				graph[i][j] = Integer.parseInt(str[j]);
+			}
+		}
+
+		visited[0][0] = true;
+		bfs(0,0);
+		
+		System.out.println(graph[n-1][m-1]);
+	
+		br.close();
+	}
+
+	public static void bfs(int y, int x) {
+		Queue<Integer> qx = new LinkedList<Integer>();
+		Queue<Integer> qy = new LinkedList<Integer>();
+		//System.out.println(x + " " + y);
+		
+		qx.add(x);
+		qy.add(y);
+
+		int tmpx;
+		int tmpy;
+
+		while (!qx.isEmpty() && !qy.isEmpty()) {
+			tmpx = qx.poll();
+			tmpy = qy.poll();
+
+			for (int i = 0; i < 4; i++) {
+				x = tmpx + dx[i];
+				y = tmpy + dy[i];
+
+				if (x >= 0 && y >= 0 && x < m && y < n) {
+					if (graph[y][x] == 1 && visited[y][x] == false) {
+						qx.add(x);
+						qy.add(y);
+						graph[y][x] = graph[tmpy][tmpx] + 1;
+						visited[y][x] = true;
+					}
+				}
+			}
+
+		}
+	}
+}
